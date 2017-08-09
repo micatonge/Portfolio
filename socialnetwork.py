d8ea0bb8122f7ec6349a00e06f141fe5a4d89604
micatonge <micatonge12@gmail.com>
2017-08-09 16:02:16 -0400

    Add files via upload

---
+++ socialnetwork.py
@@ -0,0 +1,25 @@
+import turtle
+import math
+class User:
+    def __init__(self,name):
+        self.user_name = name
+        self.friends = []
+    def user_name(self,name):
+    def add.friends(self,friends):
+        self.friends.append(friends)
+    def delete.friends(self,friends)
+        self.friends.
+
+class Network:
+    # Define the fields and methods for your object here.
+    def __init__(self,):
+
+
+
+def main():
+    # Define the program flow for your user interface here.
+
+
+# Runs your program.
+if __name__ == '__main__':
+    main():
