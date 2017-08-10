ec5dc5bd9ab169e8b8753c31aa73b056c65ee73c
micatonge <micatonge12@gmail.com>
2017-08-10 15:04:49 -0400

    Add files via upload

--- socialnetwork.py
+++ socialnetwork.py
@@ -1,25 +1,49 @@
 import turtle
 import math
 class User:
-    def __init__(self,name):
-        self.user_name = name
+    def __init__(self,user_name,user_id):
+        self.user_name = user_name
         self.friends = []
-    def user_name(self,name):
-    def add.friends(self,friends):
+        self.user_id = user_id
+    def getUsername(self):
+        return self.user_name
+    def addfriends(self,friends):
         self.friends.append(friends)
-    def delete.friends(self,friends)
-        self.friends.
+    def getFriends(self):
+        return self.friends
+    def getUserid(self):
+        return self.user_id
 
-class Network:
+class Network:s
     # Define the fields and methods for your object here.
-    def __init__(self,):
-
+    def __init__(self,user_name):
+        self.users = []
+        self.user_name = user_name
+    def num_users(self):
+        return len(self.users)
+    def getUsername(self):
+            return self.user_name
+    def addUsers(self,user_name):
+        for user_name in self.users:
+            if user_name == self.getUsername():
+                print("Sorry, that name is taken. Try again.")
+                return
+        user_id = len(self.users)
+        self.users.append(User(username, user_id))
+        return self.users
 
 
 def main():
     # Define the program flow for your user interface here.
+    myNetwork = Network()
+    mica = User("mica",1)
+    meghan = User("meghan",2)
+    friends = ["meghan","rose","nick","leyla","lena","CT"]
+    myNetwork.addUsers(mica)
+    myNetwork.addUsers(meghan)
+
 
 
 # Runs your program.
 if __name__ == '__main__':
-    main():
+    main()
